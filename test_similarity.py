8423fc48c57a8d75c0d598ae5fd3d80ce19a4c2a
hanbee.choi <hubaiwoong@proton.me>
2024-06-13 16:08:46 +0900

    [feature] add test case

--- test_similarity.py
+++ test_similarity.py
@@ -7,3 +7,18 @@ class TestSimilarity(TestCase):
         similarity_checker = Similarity()
         result = similarity_checker.score('AAA', 'DSA')
         self.assertEqual(60, result)
+
+    def test_similarity_twice(self):
+        similarity_checker = Similarity()
+        result = similarity_checker.score('A', 'BB')
+        self.assertEqual(0, result)
+
+    def test_similarity_3(self):
+        similarity_checker = Similarity()
+        result = similarity_checker.score('AAABB', 'BAA')
+        self.assertEqual(20.000000000000004, result)
+
+    def test_similarity_4(self):
+        similarity_checker = Similarity()
+        result = similarity_checker.score('AA', 'AAE')
+        self.assertEqual(30, result)
