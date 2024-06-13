8a5ae1d5ca4f100bd66e69374f7d25a80c7bbe71
hanbee.choi <hubaiwoong@proton.me>
2024-06-13 16:37:33 +0900

    [feature] add alpha based score

--- similarity.py
+++ similarity.py
@@ -7,3 +7,15 @@ class Similarity:
         B = len(b) if len(a) > len(b) else len(a)
         Gap = A - B
         return (1 - Gap / B) * 60
+
+    def alpha_based_length(self, a, b):
+        set1 = set(a)
+        set2 = set(b)
+        TotalCnt = len(set1.union(set2))
+
+        SameCnt = 0
+        for elem in set1:
+            if elem in set2:
+                SameCnt += 1
+
+        return (SameCnt / TotalCnt) * 40
--- test_similarity.py
+++ test_similarity.py
@@ -7,12 +7,16 @@ class TestSimilarity(TestCase):
         super().setUp()
         self.similarity_checker = Similarity()
 
-    def test_length_similarity(self):
+    def test_similarity(self):
         self.assertEqual(60, self.similarity_checker.score_based_length('AAA', 'DSA'))
+        self.assertEqual(40, self.similarity_checker.alpha_based_length('ASD', 'DSA'))
 
-    def test_length_similarity_zero_score(self):
+    def test_similarity_zero_score(self):
+        self.assertEqual(0, self.similarity_checker.score_based_length('A', 'BB'))
         self.assertEqual(0, self.similarity_checker.score_based_length('A', 'BB'))
 
-    def test_length_similarity_subscore_case(self):
+    def test_similarity_subscore_case(self):
         self.assertEqual(20.000000000000004, self.similarity_checker.score_based_length('AAABB', 'BAA'))
+        self.assertEqual(40, self.similarity_checker.alpha_based_length('AAABB', 'BAA'))
         self.assertEqual(30, self.similarity_checker.score_based_length('AA', 'AAE'))
+        self.assertEqual(20, self.similarity_checker.alpha_based_length('AA', 'AAE'))
