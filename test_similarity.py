786e8f2bfd09aeed2ce87266b8d737e5535677f2
hanbee.choi <hubaiwoong@proton.me>
2024-06-13 16:10:29 +0900

    [refactoring] method naming

--- test_similarity.py
+++ test_similarity.py
@@ -8,17 +8,17 @@ class TestSimilarity(TestCase):
         result = similarity_checker.score('AAA', 'DSA')
         self.assertEqual(60, result)
 
-    def test_similarity_twice(self):
+    def test_similarity_zero_score(self):
         similarity_checker = Similarity()
         result = similarity_checker.score('A', 'BB')
         self.assertEqual(0, result)
 
-    def test_similarity_3(self):
+    def test_similarity_subscore_case_1(self):
         similarity_checker = Similarity()
         result = similarity_checker.score('AAABB', 'BAA')
         self.assertEqual(20.000000000000004, result)
 
-    def test_similarity_4(self):
+    def test_similarity_subscore_case_2(self):
         similarity_checker = Similarity()
         result = similarity_checker.score('AA', 'AAE')
         self.assertEqual(30, result)
