3df42fca12558318605ba85a2f2fa75031c4ff93
hanbee.choi <hubaiwoong@proton.me>
2024-06-13 16:12:38 +0900

    [refactoring] method extract and inline

--- test_similarity.py
+++ test_similarity.py
@@ -3,22 +3,16 @@ from similarity import Similarity
 
 
 class TestSimilarity(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.similarity_checker = Similarity()
+
     def test_similarity(self):
-        similarity_checker = Similarity()
-        result = similarity_checker.score('AAA', 'DSA')
-        self.assertEqual(60, result)
+        self.assertEqual(60, self.similarity_checker.score('AAA', 'DSA'))
 
     def test_similarity_zero_score(self):
-        similarity_checker = Similarity()
-        result = similarity_checker.score('A', 'BB')
-        self.assertEqual(0, result)
+        self.assertEqual(0, self.similarity_checker.score('A', 'BB'))
 
-    def test_similarity_subscore_case_1(self):
-        similarity_checker = Similarity()
-        result = similarity_checker.score('AAABB', 'BAA')
-        self.assertEqual(20.000000000000004, result)
-
-    def test_similarity_subscore_case_2(self):
-        similarity_checker = Similarity()
-        result = similarity_checker.score('AA', 'AAE')
-        self.assertEqual(30, result)
+    def test_similarity_subscore_case(self):
+        self.assertEqual(20.000000000000004, self.similarity_checker.score('AAABB', 'BAA'))
+        self.assertEqual(30, self.similarity_checker.score('AA', 'AAE'))
